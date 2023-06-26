e8d7440cf57705d2464f928cd0939283757e11fe
Mihaela Osmajić <elaosmajic@gmail.com>
2023-06-26 20:03:30 +0200

    seed data

--- kjar-example - 2/kjar-example/drools-spring-app/src/main/java/sbnz/integracija/example/repository/AccountPackageRepository.java
+++ kjar-example - 2/kjar-example/drools-spring-app/src/main/java/sbnz/integracija/example/repository/AccountPackageRepository.java
@@ -3,7 +3,10 @@ package sbnz.integracija.example.repository;
 import demo.facts.AccountPackage;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -29,6 +32,13 @@ public class AccountPackageRepository {
     private void initPackages(){
         packages = new ArrayList<>();
 
+        Date r = new Date();
+        r.setYear(2024);
+        AccountPackage pack = new AccountPackage(UUID.fromString("0018db5f-4c4d-49a3-bd9c-d497b16cd202"),"lazar@gmail.com", "Lazar","Lazar", 11111,11112,123,5000.0,r);
+        AccountPackage pack2 = new AccountPackage(UUID.fromString("80bfb86e-2190-4cd8-9b0a-c3f308d5cfa7"),"banka@gmail.com", "Banka","Banka", 33333,33333,123,5000.0,r);
+
+        packages.add(pack);
+        packages.add(pack2);
     }
 
     public List<AccountPackage> getPackages(){
